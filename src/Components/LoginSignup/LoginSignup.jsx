236fe272e5e7c0b25d30a396cb83340d620634f2
Chaz Dooley <80858674+c-engineering-dooley@users.noreply.github.com>
2024-09-27 20:10:16 -0400

    changes

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -11,7 +11,7 @@ import password from '../Assets/password.png'
 
 const LoginSignup = () => {
 
-    const [action, setAction] = useState("Sign Up");
+    const [action, setAction] = useState("Login");
 
   return (
     <div className = 'container'>
@@ -20,31 +20,38 @@ const LoginSignup = () => {
             <div className = "underline"> </div>
         </div>
         <div className = "inputs">
+            {action === "Login"? <div></div>: 
             <div className = "input">
                 <img src={firstname} alt="" />
-                <input type="first-name" />
-            </div>
+                <input placeholder = "First Name" type="first-name" />
+            </div>}
+            {action === "Login"? <div></div>:
             <div className = "input">
                 <img src={lastname} alt="" />
-                <input type="last-name" />
-            </div>
+                <input placeholder = "Last Name" type="last-name" />
+            </div>}
+            {action === "Login"? <div></div>:
             <div className = "input">
                 <img src={email} alt="" />
-                <input type="email" />
-            </div>
+                <input placeholder = "Email" type="email" />
+            </div>}
+            {action === "Login"? <div></div>:
             <div className = "input">
                 <img src={address} alt="" />
-                <input type="address" />
-            </div>
+                <input placeholder = "Preferred Address" type="address" />
+            </div>}
+            {action === "Login"? <div></div>:
             <div className = "input">
                 <img src={DOB} alt="" />
-                <input type="date-of-birth" />
-            </div>
+                <input placeholder = "Date of Birth" type="date-of-birth" />
+            </div>}
         </div>
-        <div className = "forgot-password">Forgot Password? <span>Click Here!</span></div>
+
+        {action === "Sign Up"? <div></div>:
+        <div className = "forgot-password">Forgot Password? <span>Click Here!</span></div>}
         <div className = "submit-container">
-            <div className = {action==="Login"?"submit gray":"submit"}>Sign Up</div>
-            <div className = {action==="Sign Up"?"submit gray":"submit"}>Login</div>
+            <div className = {action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+            <div className = {action==="Sign Up"?"submit gray":"submit"}onClick={()=>{setAction("Login")}}>Login</div>
         </div>
     </div>
   )
